d0b5fbf43fbf13b9183974365cfbaa53ee892e06
Meagan Moore <816moore@gmail.com>
2018-04-15 23:02:28 -0400

    added images, animations

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import Board from './Board.js';
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">Talk to your code</h1>
           <p className="App-intro">
             Having trouble getting your code to do what you want it to? Simply paste your code into the input below and speak with it from beyond the complier!
@@ -18,7 +18,10 @@ class App extends Component {
 
         <Board />
 
-        <footer>&copy; <a href="http://www.mdjm.io">Meagan Moore</a></footer>
+        <footer>&copy; 
+          <a href="http://www.mdjm.io">Meagan Moore</a>
+          <p>sun & moon by Marco Livolsi from the Noun Project</p>
+        </footer>
       </div>
     );
   }
--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Board.css';
 
+
 class Board extends Component {
     render() {
         return (
@@ -8,12 +9,17 @@ class Board extends Component {
                 <div className="oujia-board">
                     <h2>Code Oujia</h2>
                     <h5>&copy; mdjm</h5>
-                    <span>truthy</span>
-                    <span>falsey</span>
-                    <span>You're missing a semicolon</span>
-                    <span>Try a Hard Refresh</span>
-                    <span>Restart the Complier</span>
-                    <span>Check yuor spellign</span>
+                    <img src={require('./assets/sun.png')} className="sun-top" alt="Black icon of a sun with rays" />
+                    <img src={require('./assets/moon.png')} className="moon-top" alt="Black icon of a waxing cresent moon" />
+                    <p className="truthy">truthy</p>
+                    <p className="falsey">falsey</p>
+                    <p className="semicolon">You're missing a semicolon</p>
+                    <p className="refresh">Try a Hard Refresh</p>
+                    <p className="complier">Restart the Complier</p>
+                    <p className="spelling">Check yuor spellign</p>
+                    <p className="google">Try Google</p>
+                    <img src={require('./assets/sun.png')} className="sun-bottom" alt="Black icon of a sun with rays" />
+                    <img src={require('./assets/moon.png')} className="moon-bottom" alt="Black icon of a waxing cresent moon" />
                 </div>
             </div>
         );
